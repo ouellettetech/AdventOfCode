dd1e3dd97283d447a12c9d623e7b1b895adcca57
Bradley Ouellette <Bradley.Ouellette@prometheanworld.com>
2018-12-03 18:07:19 -0800

    Day 1 of 2018

---
+++ 2018/day1.js
@@ -0,0 +1,42 @@
+input = document.body.innerText.split("\n");
+input.pop();// remove the final html character.
+
+
+console.log("Part B");
+
+function day01b(adjustments){
+    var found=false;
+    var seenValues=[];
+    var curValue=0;
+    while(!found){
+        adjustments.forEach(function(element) {
+            if(!found){ // JavaScript doesn't support break, and the performance hit from looping through one isn't a concern. (vs, a for loop with two conditions)
+                if(seenValues.includes(curValue)){
+                    console.log("found dup");
+                    console.log(curValue);
+                    found=true;
+                } else {
+                    seenValues.push(curValue);
+                    curValue+=parseInt(element);
+                }
+            }
+        });
+    }
+}
+
+function day01a(adjustments){
+    var curValue=0;
+    // I first solved Part A, by just pasting all the values into the developer console, since it just requires adding all the values.
+    adjustments.forEach(function(element) {
+
+        //console.log(parseInt(element));
+        curValue+=parseInt(element);
+    });
+    console.log(curValue);
+}
+
+//testData();
+console.log("Day 01 Part A");
+day01a(input);
+console.log("Day 24 Part B");
+day01b(input);
